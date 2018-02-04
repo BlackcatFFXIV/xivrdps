ee0848658ec67bb1438536c0736b1f7b3c26411f
BlackcatFFXIV <blackcathonks@gmail.com>
2018-02-04 03:46:38 -0500

    Added Byakko to the encounter list.

--- fflogs-resources.js
+++ fflogs-resources.js
@@ -100,7 +100,7 @@ const FFLogsResources = {
     {name: 'Sigmascape (Savage)', encounters: {'Phantom Train': '51', 'Demon Chadarnook': '52', 'Guardian': '53', 'Kefka': '54', 'God Kefka': '55'}},
     {name: 'Ultimate', encounters: {'Unending Coil': '1039'}},
     {name: 'Deltascape (Savage)', encounters: {'Alte Roite': '42', 'Catastrophe': '43', 'Halicarnassus': '44', 'Exdeath': '45', 'Neo Exdeath': '46'}},
-    {name: 'Trials', encounters: {'Susano': '1036', 'Lakshmi': '1037', 'Shinryu': '1038'}},
+    {name: 'Trials', encounters: {'Susano': '1036', 'Lakshmi': '1037', 'Shinryu': '1038', 'Byakko': '1040'}},
     {name: 'Rabanastre', encounters: {'Mateus, the Corrupt': '2008', 'Hashmal, Bringer of Order': '2009', 'Rofocale': '2010', 'Argath Thadalfus': '2011'}}
   ],
 
