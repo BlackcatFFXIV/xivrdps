75f9f7968e0d0b1d2f414e77edc65d96ac663859
BlackcatFFXIV <blackcathonks@gmail.com>
2018-09-08 01:02:03 -0400

    .

--- views.js
+++ views.js
@@ -2,7 +2,7 @@ const resources = require('./fflogs-resources')
 const changeLog = require('./change-log')
 const Result = require('./models/result')
 const RaidDPSPipeline = require('./raid-dps-pipeline')
-const debug = true
+const debug = false
 const dateOptions = {year: "numeric", month: "long", day: "numeric"}
 const pipelines = {}
 
